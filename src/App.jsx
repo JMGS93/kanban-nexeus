dc407a6cb04090219e969a5968ae5113c928c00f
Jose Manuel Garcia Segura <pontaeri18@gmail.com>
2025-11-13 11:53:09 +0100

    Añadido AppTour con tooltip personalizado en español

--- src/App.jsx
+++ src/App.jsx
@@ -362,7 +362,7 @@ function App() {
               </button>
 
               <button
-                className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-medium px-4 py-2 rounded shadow-md flex items-center justify-center gap-2"
+                className="bg-yellow-400 hover:bg-yellow-500 text-white font-medium px-4 py-2 rounded shadow-md flex items-center justify-center gap-2"
                 onClick={() => {
                   localStorage.removeItem("tutorialCompleted");
                   setStartTour(true);
